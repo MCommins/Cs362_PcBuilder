051f8b5ec392a869df34dc5b9326683b12ad5e5c
Devon Crane <cranede@10-116-29-78.wireless.oregonstate.edu>
2019-05-05 14:39:57 -0700

    sprint 5 turn in

--- app/models/part.rb
+++ app/models/part.rb
@@ -3,4 +3,50 @@ class Part < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :part, presence: true
   validates :compatability, presence: true, numericality: { greater_than: 0 }
+
+  def compare (parta, partb)
+    if(parta == nil || partb == nil)
+      return false;
+    else 
+      if(parta.name == name || partb.name == name || parta.name == partb.name)
+        return false;
+      else
+        if(parta.part == part || partb.part == part || parta.part == partb.part)
+          return false;
+        else
+          if(parta.compatability != compatability || partb.compatability != compatability || parta.compatability != partb.compatability)
+            return false;
+          end
+        end
+      end
+    end
+    return true;
+  end
+        
 end
+
+=begin
+
+
+  if(parta == NULL || partb == NULL)
+      return false;
+    else
+      if(parta.name == name || partb.name == name)
+        return false;
+      else 
+        if(parta.part == part || partb.part == part)
+          return false;
+        else  
+          if (parta.compatability == compatability && partb.compatability == compatability)
+            return true;
+          end
+        end
+      end
+    end
+    
+    return true;
+  end
+        
+end
+
+=end
--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Part, type: :model do
-  
+
+  it "doesn't allow a NULL input" do
+    parta = Part.new(name: "fake", part: "part", compatability: 1)
+    partb = Part.new(name: "name", part: "part", compatability: 1)
+    expect(parta.compare(partb,nil)).to be_falsey
+  end
+
+  it "doesn't allow parts with the same name" do
+    parta = Part.new(name: "name", part: "part", compatability: 1)
+    partb = Part.new(name: "fake", part: "part", compatability: 1)
+    partc = Part.new(name: "fake", part: "part", compatability: 1)
+    expect(parta.compare(partb,partc)).to be_falsey
+  end
+
+  it "doesn't allow parts with the same part type" do
+    parta = Part.new(name: "amd", part: "part", compatability: 1)
+    partb = Part.new(name: "asus", part: "part1", compatability: 1)
+    partc = Part.new(name: "gygabite", part: "part1", compatability: 1)
+    expect(parta.compare(partb,partc)).to be_falsey
+  end
+
+  it "doesn't allow parts that don't share the same compatability number" do
+    parta = Part.new(name: "amd", part: "part1", compatability: 1)
+    partb = Part.new(name: "asus", part: "part2", compatability: 2)
+    partc = Part.new(name: "gygabite", part: "part3", compatability: 3)
+    expect(parta.compare(partb,partc)).to be_falsey
+  end 
+
+  it "This is the happy path where all parts names and types are diffrent but compatability number is the same" do
+    parta = Part.new(name: "amd", part: "part1", compatability: 1)
+    partb = Part.new(name: "asus", part: "part2", compatability: 1)
+    partc = Part.new(name: "gygabite", part: "part3", compatability: 1)
+    expect(parta.compare(partb,partc)).to be_truthy
+  end
 end
