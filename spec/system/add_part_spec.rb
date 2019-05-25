3df5318e2ed1a4fd44b85dcf60944bfeb958e40d
Ethan Jones <joneetha@oregonstate.edu>
2019-05-24 18:08:40 -0700

    Fix formatting in part spec

--- spec/system/add_part_spec.rb
+++ spec/system/add_part_spec.rb
@@ -21,13 +21,13 @@ RSpec.describe "adding parts to the list", type: :system do
     fill_in "Compatibility", with: 3
     click_on("Create Part")
 	
-	visit new_part_path
+  	visit new_part_path
     fill_in "Name", with: "ASUS X470"
     fill_in "Part", with: "Motherboard"
     fill_in "Compatibility", with: 7
     click_on("Create Part")
 	
-	expect(page).to have_content("AMD Ryzen 2700X")
+	  expect(page).to have_content("AMD Ryzen 2700X")
     expect(page).to have_content("CPU")
     expect(page).to have_content(3)
 	
@@ -53,7 +53,7 @@ RSpec.describe "adding parts to the list", type: :system do
   end
   
   it "does not allow the user to add a nonsensical compatibility number" do
-	visit new_part_path
+	  visit new_part_path
     fill_in "Name", with: "ASUS X470"
     fill_in "Part", with: "Motherboard"
     fill_in "Compatibility", with: -1
@@ -63,7 +63,7 @@ RSpec.describe "adding parts to the list", type: :system do
   end
   
   it "does not allow the user to add a non-integer compatibility number" do
-	visit new_part_path
+	  visit new_part_path
     fill_in "Name", with: "ASUS X470"
     fill_in "Part", with: "Motherboard"
     fill_in "Compatibility", with: "TEST"
@@ -79,7 +79,7 @@ RSpec.describe "adding parts to the list", type: :system do
     fill_in "Compatibility", with: 7
     click_on("Create Part")
 	
-	visit new_part_path
+  	visit new_part_path
     fill_in "Name", with: "asus x470"
     fill_in "Part", with: "Motherboard"
     fill_in "Compatibility", with: 15
