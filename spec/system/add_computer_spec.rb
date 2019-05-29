6503bba47c03393661462b6c924e6b8cd250e844
Ethan Jones <joneetha@oregonstate.edu>
2019-05-29 10:50:44 -0700

    Add tests for making computers with parts

--- spec/system/add_computer_spec.rb
+++ spec/system/add_computer_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Computer, type: :system do
-  it "allows the user to make a computer" do
+  it "allows the user to make a computer with no parts" do
     visit new_part_path
     fill_in "Name", with: "Saphire Tech"
     fill_in "Part", with: "Graphics Card"
@@ -8,11 +8,40 @@ RSpec.describe Computer, type: :system do
     visit new_computer_path
     fill_in "Name", with: "Cool Computer"
     fill_in "Owner", with: "Me"
-    select "Saphire Tech", from: "part_1"
-    select "Saphire Tech", from: "part_2"
-    select "Saphire Tech", from: "part_3"
     click_on("Create Computer")
     expect(page).to have_content("Cool Computer")
     expect(page).to have_content("Me")
   end
+
+  it "allows making a computer with many different parts" do
+    visit new_part_path
+    fill_in "Name", with: "Narrow sieve"
+    fill_in "Part", with: "Slime processor"
+    fill_in "Compatibility", with: 3
+    click_on("Create Part")
+
+    visit new_part_path
+    fill_in "Name", with: "Mysterious torus"
+    fill_in "Part", with: "Particle collider"
+    fill_in "Compatibility", with: 3
+    click_on("Create Part")
+
+    visit new_part_path
+    fill_in "Name", with: "Tall obelisk"
+    fill_in "Part", with: "Alien antenna"
+    fill_in "Compatibility", with: 3
+    click_on("Create Part")
+
+    visit new_computer_path
+    fill_in "Name", with: "Cool Computer"
+    fill_in "Owner", with: "Me"
+    select "Narrow sieve", from: "part_1"
+    select "Mysterious torus", from: "part_2"
+    select "Tall obelisk", from: "part_3"
+    click_on("Create Computer")
+
+    expect(page).to have_content("Cool Computer")
+    expect(page).to have_content("Me")
+  end
+
 end
