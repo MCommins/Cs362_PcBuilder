38610602d645ecce14ccafe9958bf3465dbbca6a
Ethan Jones <joneetha@oregonstate.edu>
2019-05-30 01:20:01 -0700

    Add test for CreatesComputer with many parts

--- spec/workflows/creates_computer_spec.rb
+++ spec/workflows/creates_computer_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe CreatesComputer do
     expect(my_computer.computer.parts.size).to be 2
   end
 
+  it "creates a computer with a ton of parts" do
+    me = Account.create(name: "Me")
+
+    parts = []
+    (1..20).each do |i|
+      parts << Part.create(name: "Widget #{i}", part: "Widget", compatibility: 1, account: me)
+    end
+
+    my_computer = CreatesComputer.new(
+      account: me.id,
+      parts: parts)
+    my_computer.create
+    expect(my_computer.success?).to be true
+    expect(my_computer.computer.parts.size).to be 20
+  end
+
+
+
 end
